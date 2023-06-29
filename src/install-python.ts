60c2db2f4f23944ade020e6250ff951abee15ce1
Gaurang Gunde <gaurang.gunde@thinkdeca.com>
2023-06-29 04:10:50 -0700

    fix: Update python-versions repo to point to Deca's local

--- src/install-python.ts
+++ src/install-python.ts
@@ -7,7 +7,7 @@ import { IS_WINDOWS, IS_LINUX } from './utils';
 
 const TOKEN = core.getInput('token');
 const AUTH = !TOKEN ? undefined : `token ${TOKEN}`;
-const MANIFEST_REPO_OWNER = 'actions';
+const MANIFEST_REPO_OWNER = 'Deca-Technologies';
 const MANIFEST_REPO_NAME = 'python-versions';
 const MANIFEST_REPO_BRANCH = '225ba42747d0f5e3dbd90ba15b9c7409a4b8c735';
 export const MANIFEST_URL = `https://raw.githubusercontent.com/${MANIFEST_REPO_OWNER}/${MANIFEST_REPO_NAME}/${MANIFEST_REPO_BRANCH}/versions-manifest.json`;
